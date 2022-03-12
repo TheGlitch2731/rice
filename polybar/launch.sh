2597811b553620d2dbbb0017213140af820932c5
TheGlitch2731 <79125737+TheGlitch2731@users.noreply.github.com>
2022-03-13 00:04:09 +0530

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,15 @@
-!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch Polybar, using default config location ~/.config/polybar/config
+for screen in $(bspc query -M --names)
+do
+    polybar $screen &
+done
 
-echo "Bars launched..."
+echo "Polybar launched..."
